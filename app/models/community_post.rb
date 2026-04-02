26c9abb62dc9759ea38eb9fb1a05e040eb3d7f9d
Jeffery LAU <jefferylau@jefferydeMacBook-Pro.local>
2026-04-03 03:27:48 +0800

    Better UI design for Community page following the website theme

--- app/models/community_post.rb
+++ app/models/community_post.rb
@@ -2,5 +2,6 @@ class CommunityPost < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :content, presence: true
+  validates :title, presence: true
   validates :title, length: { maximum: 100 }, allow_blank: true
 end
